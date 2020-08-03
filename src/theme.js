1bbaf841b14bdea42d945ea4460a8d7dc1fccbde
Thomas <thomas.hawkins@thinkresearch.com>
2020-08-03 01:10:14 -0400

    Add hooks to canvas component, add theme provider for styles

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    black: 'rgb(27, 28, 29)',
+    error: 'rgb(219, 40, 39)',
+  },
+  fonts: {
+    bodyText: '"Helvetica Neue", Arial',
+  },
+  fontSizes: {
+    modalHeader: '16px',
+    modalButton: '12px',
+    base: '13px',
+  },
+};
+
+export default theme;
