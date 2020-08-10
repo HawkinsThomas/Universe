022457a8f470fde6af54214354d920dfd197cc2d
Thomas <thomas.hawkins@thinkresearch.com>
2020-08-10 13:28:34 -0400

    Add framerate throttle

--- src/pages/CanvasPage/Canvas/component.js
+++ src/pages/CanvasPage/Canvas/component.js
@@ -10,14 +10,29 @@ const Canvas = () => {
   const numberOfCircles = 1000;
   let frameNumber = 0;
   const frames = createRandomFrames(window.innerHeight, window.innerWidth, numberOfCircles, numberOfFrames);
+  const fps = 20;
+  const fpsInterval = 1000 / fps;
+  let prevTime;
 
+  const startAnimation = () => {
+    prevTime = window.performance.now();
+    animate();
+  }
 
-  const animate = () => {
+  const animate = (newtime) => {
     requestAnimationFrame(animate);
-    clearCanvas(canvasRef);
-    drawCircles(canvasRef, frames[frameNumber]);
-    frameNumber += 1;
-    frameNumber %= numberOfFrames;
+
+    let now = newtime;
+    let timeElapsed = now - prevTime;
+
+    if (timeElapsed > fpsInterval) {
+      prevTime = now - (timeElapsed % fpsInterval)
+
+      clearCanvas(canvasRef);
+      drawCircles(canvasRef, frames[frameNumber]);
+      frameNumber += 1;
+      frameNumber %= numberOfFrames;
+    }
   }
 
   return(
@@ -25,7 +40,7 @@ const Canvas = () => {
       ref={canvasRef}
       width={window.innerWidth}
       height={window.innerHeight}
-      onClick={animate}
+      onClick={() => startAnimation()}
     />
   );
 }
